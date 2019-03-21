d2fcd0d0f2550a9c3b466c479411213ed3f0130a
Dolly Chen <dollychen@Dollys-Air.home>
2019-03-20 21:42:44 -0400

    problem solving tutorial

--- hello.js
+++ hello.js
@@ -1,7 +1,19 @@
 var x = 1;
 var y = 2;
 
-y = x + y;
+y = x * y;
 
 console.log(x);
 console.log(y);
+
+var avalon = 10;
+var patron = 4;
+
+//console.log (avalon*x % patron*y);
+
+avalon = (avalon*x) % (patron*y);
+
+console.log (avalon);
+console.log (patron);
+
+
